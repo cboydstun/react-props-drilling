358909fc472c2545fb4f9ce8110c893589400b58
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-05-24 07:23:36 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Names from './components/Names'
+
+export default function App() {
+    return (
+        <div>
+            <Names />
+        </div>
+    )
+}
---
+++ src/components/Names.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Names() {
+
+
+
+    return (
+        <div>
+            hey from  names
+        </div>
+    )
+}
